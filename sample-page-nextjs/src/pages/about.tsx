ac4384b932c19e8fc0b4f7a8fd0004993771ddec
Alex <alex.suarez@lemonade.com>
2022-11-01 18:58:02 +0100

    update about us component next

--- sample-page-nextjs/src/pages/about.tsx
+++ sample-page-nextjs/src/pages/about.tsx
@@ -1,9 +1,7 @@
-import { Navigation } from '../Layout/Navigation';
+import { AboutUs } from '../components/AboutUs';
 import { NextPage } from 'next';
-import { useState } from 'react';
 
 const About: NextPage = () => {
-  const [state, setState] = useState(0);
   return (
     <>
       <h1 className="font-bold text-xl">Hello About</h1>
@@ -13,18 +11,7 @@ const About: NextPage = () => {
         alias quo fuga reprehenderit dolores saepe autem repudiandae!
       </p>
 
-      <div className="flex gap-3">
-        <button
-          className="h-10 px-4 bg-red-500 text-white rounded mt-8"
-          onClick={() => setState(0)}>
-          Reset counter
-        </button>
-        <button
-          className="h-10 px-4 bg-indigo-500 text-white rounded mt-8"
-          onClick={() => setState((current) => current + 1)}>
-          Count <b>{state}</b>
-        </button>
-      </div>
+      <AboutUs />
     </>
   );
 };
